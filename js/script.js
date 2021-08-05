57967a33a41910790c696d8db7e9e97f76674bf1
nozkit <88146299+nozkit@users.noreply.github.com>
2021-08-06 02:57:04 +0900

    commit 08.05

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 $(function() {
+
+
+
+
     // ハンバーガーメニュ
     $('.menu').on('click',function(){
         $('.menu__line').toggleClass('active');
         $('.gnav').fadeToggle();
     });
     // ハンバーガーメニュ
+
+    var winH = $(window ).height();
+      $('.top').outerHeight($(window).height());    
+      $(window).on('resize',function(){
+        winH = $(window).height();
+        $('.top').outerHeight(winH);
+      });
+
+    if(window.matchMedia("(max-width: 767px)").matches){
+      $('.top').css({
+        'height':'-=80px'
+        });
+    }else{
+      $('.top').css({
+        'height':'-=200px'
+        });
+    }
+
+    
   });
 
