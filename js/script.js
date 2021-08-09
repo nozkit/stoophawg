c18b9ade5978f5a7875e7662f7030d2576a408f4
nozkit <88146299+nozkit@users.noreply.github.com>
2021-08-10 01:54:49 +0900

    08.09.01

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
-$(function() {
+$(function () {
 
 
-    var winH = $(window ).height();
-      $('.top').outerHeight($(window).height());    
-      $(window).on('resize',function(){
-        winH = $(window).height();
-        $('.top').outerHeight(winH);
-      });
-
-    if(window.matchMedia("(max-width: 767px)").matches){
-      $('.top').css({
-        'height':'-=80px'
-        });
-    }else{
-      $('.top').css({
-        'height':'-=200px'
-        });
-    }
-
-    
+  var winH = $(window).height();
+  $('.top').outerHeight($(window).height());
+  $(window).on('resize', function () {
+    winH = $(window).height();
+    $('.top').outerHeight(winH);
   });
+
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    $('.top').css({
+      'height': '-=80px'
+    });
+  } else {
+    $('.top').css({
+      'height': '-=220px'
+    });
+  }
+
+
+  // header
+  var $win = $(window),
+    $header = $('header'),
+    animationClass = 'is-animation';
+
+  $win.on('load scroll', function () {
+    var value = $(this).scrollTop();
+    if (value > 300) {
+      $header.addClass(animationClass);
+    } else {
+      $header.removeClass(animationClass);
+    }
+  });
+  // header
+
+  // NavBar
+  
+  // NavBar
+
+});
 
