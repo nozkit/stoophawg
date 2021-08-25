b062439ec712956012063644f64cb188913179a2
nozkit <88146299+nozkit@users.noreply.github.com>
2021-08-26 03:42:14 +0900

    08.25.01

--- js/script.js
+++ js/script.js
@@ -85,5 +85,24 @@ $(function () {
   });
   // リンク位置調整
 
+  $(window).on('load scroll', function (){
+
+    var box = $('.titleBox');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      //画面内のどの位置で処理を実行するかで「100」の値を変更
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
 });
 
